b5aedf3841ba373a6dc7b789d1691bc57f961bdc
Alberto Pedraza <washburnuser@gmail.com>
2026-09-01 12:58:47 -0600

    Se agrega la tarea 7 con algunos intentos, cabe señalar que no son todas las maneras en que se pueden cumplir algunas de las indicaciones

--- src/task7.js
+++ src/task7.js
@@ -5,7 +5,22 @@ const arr = [
   ]
   
   // Type your code below this line!
+  //*Agregar un solo número a una fila existente
+  arr[0].push(10); //Aquí hacemos que se anexe el número 10 al final de la primera fila del array
+  console.log(arr);
+
+  //*Agregar una fila completa de números
+  arr.push([30,31,32,33,34,35,36,37,38,39]);
+  console.log("========Agregar fila completa==========");
+  console.log(arr);
+
+  //* Eliminar un solo número de una fila específica
+  arr[0].splice(10,1);
+  console.log("========Eliminar un sólo número de una fila específica==========");
+  console.log(arr);
   
-  
-  
+  //* Invertir una de las filas sin afectar a las demás.
+  arr[3].reverse();
+  console.log("========Invertir el orden de una fila específica==========");
+  console.log(arr);
   // Type your code above this line!
